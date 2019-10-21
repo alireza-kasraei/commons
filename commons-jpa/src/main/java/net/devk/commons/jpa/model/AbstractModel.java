adaead6c447bb80b1035a495ac08fbf6752796de
Akasraei <kasraei.ar@gmail.com>
2019-10-21 18:05:06 +0330

    replace Date to LocalDateTime

--- commons-jpa/src/main/java/net/devk/commons/jpa/model/AbstractModel.java
+++ commons-jpa/src/main/java/net/devk/commons/jpa/model/AbstractModel.java
@@ -1,24 +1,32 @@
 package net.devk.commons.jpa.model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 
 import lombok.Data;
-import net.devk.commons.util.date.DateUtils;
 
 @Data
 @MappedSuperclass
 public abstract class AbstractModel {
 
 	@Column(name = "REGISTER_DATE")
-	private Date registerDate;
+	private LocalDateTime registerDate;
+
+	@Column(name = "UPDATE_DATE")
+	private LocalDateTime updateDate;
 
 	@PrePersist
 	public void onPrePersist() {
-		registerDate = DateUtils.now();
+		registerDate = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	public void onPreUpdate() {
+		updateDate = LocalDateTime.now();
 	}
 
 }
